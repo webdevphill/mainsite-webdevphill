df5cb32d67080696286bc5aeca72419e9e05233c
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2020-02-29 14:25:24 +1100

    start on adding blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogPostsComponent } from './pages/blog-posts/blog-posts.component';
+import { BlogPostComponent } from './pages/blog-post/blog-post.component';
+import { SitesComponent } from './pages/sites/sites.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SitesComponent },
+  {
+    path: 'blog',
+    children: [
+      { path: '', component: BlogPostsComponent },
+      { path: 'posts', component: BlogPostsComponent },
+      { path: 'post/:id', component: BlogPostComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SitesComponent } from './pages/sites/sites.component';
+import { BlogPostsComponent } from './pages/blog-posts/blog-posts.component';
+import { BlogPostComponent } from './pages/blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SitesComponent
+    SitesComponent,
+    BlogPostsComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
