69c266afa5fe68f009a0726da475a2c3e4004444
Borgosity <borgosity@gmail.com>
2020-07-17 16:35:22 +1000

    remove blog components, add header and footer components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BlogPostsComponent } from './pages/blog-posts/blog-posts.component';
-import { BlogPostComponent } from './pages/blog-post/blog-post.component';
 import { SitesComponent } from './pages/sites/sites.component';
 
 
 const routes: Routes = [
-  { path: '', component: SitesComponent },
-  {
-    path: 'blog',
-    children: [
-      { path: '', component: BlogPostsComponent },
-      { path: 'posts', component: BlogPostsComponent },
-      { path: 'post/:id', component: BlogPostComponent }
-    ]
-  }
+  { path: '', component: SitesComponent }
 ];
 
 @NgModule({
